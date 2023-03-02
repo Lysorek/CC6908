4e1cda239e35166f6c7ea183824c1f14e3f8ea46
Lysorek <pipelealcerro@gmail.com>
2023-03-02 19:06:08 -0300

    Probando funciones de pyrosm

--- Dataset/OSM/pbf_processor.py
+++ Dataset/OSM/pbf_processor.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt # Es necesario importar el plot para que se vea
 from pyrosm import get_data, OSM
 from pyrosm.data import sources
 
+
 # COMANDOS #
 # cd "Desktop/Universidad/14° Semestre (Primavera 2022)/CC6908-Introducción al Trabajo de Título/CC6909-Ayatori/Dataset/OSM"
 # python.exe pbf_processor.py
@@ -20,15 +21,27 @@ print("Filepath: ", fp)
 osm = OSM(fp)
 
 # Driving
-#drive_net = osm.get_network(network_type="driving")
-#drive_net.plot()
+#nodes, edges = osm.get_network(nodes=True, network_type="driving")
+#plt.title('Caminos disponibles en Santiago para viajar: en auto.')
+
+# Cycling
+#nodes, edges = osm.get_network(nodes=True, network_type="cycling")
+#plt.title('Caminos disponibles en Santiago para viajar: en bicicleta.')
 
 # Walking
 nodes, edges = osm.get_network(nodes=True,network_type="walking")
+plt.title('Caminos disponibles en Santiago para viajar: caminando.')
+
 ax = edges.plot()
 nodes.plot(ax = ax, color = 'orange', markersize = 2)
 
-plt.title('Caminos disponibles en Santiago para viajar: caminando.')
+#my_filter = {"building": ["residential", "retail"]}
+#buildings = osm.get_buildings(custom_filter=my_filter)
+
+#title = "Filtered buildings: " + ", ".join(buildings["building"].unique())
+#ax = buildings.plot(column="building", cmap="RdBu", legend=True)
+#ax.set_title(title);
+
 plt.xlabel('Latitud')
 plt.ylabel('Longitud')
 plt.show()
